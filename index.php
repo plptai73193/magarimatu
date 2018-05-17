00bedad809025ed3b1685040229bf59427bc7ec9
unknown <plptai73193@gmail.com>
2018-05-17 18:09:40 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, maximum-scale=1.0, user-scalable=yes" />
+	<title>Magarimatu</title>
+	<meta property="og:title" content="" />
+	<meta property="og:type" content="" />
+	<meta property="og:image" content="" />
+	<meta property="og:url" content="" />
+	<meta name="Description" content="">
+	<meta name="Keywords" content="">
+	<link rel="stylesheet" href="shared/css/common.css">
+	<link rel="stylesheet" href="shared/css/shared.css">
+	<link rel="stylesheet" href="shared/css/index.css">
+</head>
+<body style="min-height: 10000px;">
+	<div id="siteBorder">
+		<div class="top"></div>
+		<div class="right"></div>
+		<div class="bottom"></div>
+		<div class="left"></div>
+	</div>
+	<div class="headerDeco">
+		<img src="shared/img/shared/header_deco.png" alt="header deco">
+	</div>
+	<header class="clearfix">
+		<div class="logo">
+			<a href="">
+				<img src="shared/img/shared/logo.png" alt="">
+			</a>
+		</div>
+		<nav class="navMenu">
+			<ul>
+				<li><a href="/about/">曲がり松商店街とは</a></li>
+				<li><a href="/shop/">店舗</a></li>
+				<li><a href="/news/">お知らせ</a></li>
+				<li><a href="/schedule/">年間イベント</a></li>
+				<li><a href="/gpp/">ガルパンと曲がり松商店街</a></li>
+				<li><a href="/access/">アクセス</a></li>
+				<li class="contactLink"><a href="/contact/">お問合せ</a></li>
+			</ul>
+		</nav>
+	</header>
+	<div class="key">
+		<img src="shared/img/index/key.png" alt="" class="img-responsive" width="100%">
+		<h2 class="ttl">
+			<img src="shared/img/index/keyTitle.png" alt="">
+		</h2>
+	</div>
+	<div id="sec1">
+		<div class="container_16">
+			<h3 class="ttl">
+				<img src="shared/img/index/sec1_ttl.png" alt="">
+			</h3>
+			<div class="news clearfix">
+				<div class="latestNews clearfix">
+					<div class="newsItem">
+						<a href="" class="link">
+							<div class="featureImg">
+								<img src="shared/img/index/featureImg1.jpg" alt="" class="img-responsive">
+							</div>
+							<span class="date">2018/4/26</span>
+							<h4 class="newsTitle">朝マルシェについて</h4>
+						</a>
+					</div>
+				</div>
+				<div class="Newnews clearfix">
+					<div class="newsItem">
+						<a href="" class="link">
+							<div class="featureImg">
+								<img src="shared/img/index/featureImg1.jpg" alt="" class="img-responsive">
+							</div>
+							<span class="date">2018/4/26</span>
+							<h4 class="newsTitle">朝マルシェについて</h4>
+						</a>
+					</div>
+					<div class="newsItem">
+						<a href="" class="link">
+							<div class="featureImg">
+								<img src="shared/img/index/featureImg1.jpg" alt="" class="img-responsive">
+							</div>
+							<span class="date">2018/4/26</span>
+							<h4 class="newsTitle">朝マルシェについて</h4>
+						</a>
+					</div>
+					<div class="newsItem">
+						<a href="" class="link">
+							<div class="featureImg">
+								<img src="shared/img/index/featureImg1.jpg" alt="" class="img-responsive">
+							</div>
+							<span class="date">2018/4/26</span>
+							<h4 class="newsTitle">朝マルシェについて</h4>
+						</a>
+					</div>
+					<div class="newsItem">
+						<a href="" class="link">
+							<div class="featureImg">
+								<img src="shared/img/index/featureImg1.jpg" alt="" class="img-responsive">
+							</div>
+							<span class="date">2018/4/26</span>
+							<h4 class="newsTitle">朝マルシェについて</h4>
+						</a>
+					</div>
+				</div>
+			</div>
+			<a href="" class="buttonLink sec1Button">お知らせ一覧へ</a>
+			<div class="clearfix">
+				<div class="someKindofBanner">
+					<img src="shared/img/index/banner.png" alt="" class="img-responsive">
+				</div>
+				<div class="Twitter">
+					<img src="shared/img/index/twitter.jpg" alt="" class="img-responsive">
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
